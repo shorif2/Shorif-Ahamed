a5bebb0b1b5aec816c527b51ed409067ed8eb05f
Shorif <shorifahamed571@gmail.com>
2025-01-04 22:54:36 +0600

    Error page added

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,51 +1,44 @@
-import {
-    createBrowserRouter,
-  } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layout/MainLayout";
 import About from "../pages/About/About";
-import Resume from "../pages/Resume/Resume";
+import Blog from "../pages/Blog/Blog";
 import Contact from "../pages/Contact/Contact";
 import Portfolio from "../pages/Portfolio/Portfolio";
+import Resume from "../pages/Resume/Resume";
 import Skill from "../pages/Service/Skill";
-import Blog from "../pages/Blog/Blog";
 
-
-
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <MainLayout></MainLayout>,
-      errorElement: <div>error</div>,
-      children: [
-        {
-            path: '/',
-            element: <About></About>
-        },
-        {
-            path: '/resume',
-            element: <Resume></Resume>
-        },
-        {
-          path: '/contact',
-          element: <Contact></Contact>
-        },
-        {
-          path: '/portfolio',
-          element: <Portfolio></Portfolio>
-        },
-        {
-          path: '/skill',
-          element: <Skill></Skill>
-        },
-        {
-          path:'/blog',
-          element: <Blog></Blog>
-        }
-      ]
-    },
-    
-  ]);
-
-
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    errorElement: <div>error</div>,
+    children: [
+      {
+        path: "/",
+        element: <About></About>,
+      },
+      {
+        path: "/resume",
+        element: <Resume></Resume>,
+      },
+      {
+        path: "/contact",
+        element: <Contact></Contact>,
+      },
+      {
+        path: "/portfolio",
+        element: <Portfolio></Portfolio>,
+      },
+      {
+        path: "/skill",
+        element: <Skill></Skill>,
+      },
+      {
+        path: "/blog",
+        element: <Blog></Blog>,
+      },
+    ],
+  },
+]);
 
 export default router;
