203bdfc675687223e2586e566ecc18356e89baf3
shorif2 <shorifahamed571@gmail.com>
2025-01-28 23:42:09 +0600

    Update

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -101,8 +101,7 @@ const About = () => {
           reader will be distracted by the readable content of a page when
           looking at its layout. The point of using Lorem Ipsum is that it has a
           more-or-less normal distribution of letters, as opposed to using
-          Content here content here making it look like readable English. Many
-          desktop publishing packages and web model text.
+          Content here content here making it look like readable English.
         </p>
       </div>
       {/* <div>
